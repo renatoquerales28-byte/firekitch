a663f4856931e49578581db32929a32399144673
Renato Querales <renatoquerales28@gmail.com>
2026-02-27 15:22:54 -0400

    Initial commit of Firekitch landing page with Vite React TS

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-container">
+      <div className="background-glow"></div>
+      <div className="logo-container">
+        <img 
+          src="/Firekitch-Logo.svg" 
+          alt="Firekitch Logo" 
+          className="firekitch-logo" 
+        />
+      </div>
+    </main>
+  )
+}
+
+export default App
